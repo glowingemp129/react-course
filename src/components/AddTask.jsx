02b93e75f44b66486df4f252b91ed465e4c79c70
Taqi Ul Hassan <glowingemp129@glowingsoft.com>
2025-09-25 16:05:31 +0500

    Handle onChange

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,13 +1,24 @@
 import './AddTask.css';
+import {useState} from "react";
 
 export default function AddTask() {
+    const [taskName, setTaskName] = useState('');
+
+    const handleChange = (e) => {
+        setTaskName(e.target.value);
+    }
+
+    const handleReset = () => {
+        setTaskName("");
+    }
     return (
         <section className="addtask">
             <form>
-                <label htmlFor="title">Task Name:</label>
-                <input type="text" name="title" id="title" placeholder="Please Enter the Task Title" autoComplete="off" />
+                <input onChange={handleChange} type="text" name="title" id="title" placeholder="Please Enter the Task Title" autoComplete="off" value={taskName} />
                 <button type="submit">Add Task</button>
+                <span className="reset" onClick={handleReset}>Reset</span>
             </form>
+            <p>{taskName}</p>
         </section>
     )
 }
