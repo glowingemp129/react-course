20d7f5b603365c0cd82f4ade13bd41fd0c073e9e
Taqi Ul Hassan <glowingemp129@glowingsoft.com>
2025-09-25 15:07:44 +0500

    Apply Module CSS on the TaskCard Component

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,10 +1,11 @@
 import './TaskCard.css'
+import styles from './TaskCard.module.css';
 
 export default function TaskCard({ task, handleDelete }) {
     return (
         <div className="taskcard">
             <li key={task.id} className={task.completed ? "completed" : "incomplete"}>
-                <span>{task.id} - {task.name}</span>
+                <span className={styles.title}>{task.id} - </span><span>{task.name}</span>
                 <button className="delete" onClick={()=> handleDelete(task.id)}>Delete</button>
             </li>
         </div>
